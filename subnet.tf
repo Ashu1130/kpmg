4d8aea11b12cdffa940b0b8ca0e562b94eacfaf6
Ashutosh <ashutosh.jaiswal3011@gmail.com>
2023-08-04 00:46:56 +0530

    3tier commit

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.cidr[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.cidr[count.index]
   availability_zone = var.az[count.index]
-  count = 2
+  count             = 2
 
   tags = {
     Name = "public-sub"
@@ -10,8 +10,8 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
