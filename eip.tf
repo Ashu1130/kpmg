b189178c5e5be767e2e1aa36e494b04a0bcc9712
Ashutosh <ashutosh.jaiswal3011@gmail.com>
2023-08-04 20:51:12 +0530

    route table changes

---
+++ eip.tf
@@ -0,0 +1,7 @@
+resource "aws_eip" "eip-1a" {
+  domain   = "vpc"
+}
+
+resource "aws_eip" "eip-1b" {
+  domain   = "vpc"
+}
--- routetable.tf
+++ routetable.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "rtb" {
+resource "aws_route_table" "pub-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -11,9 +11,28 @@ resource "aws_route_table" "rtb" {
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "rt_associate_public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.rtb.id
   count          = 2
 }
 
+resource "aws_route_table" "pri-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+  tags = {
+    Name = "pri-rt"
+  }
+}
+resource "aws_route_table_association" "rt_associate_private" {
+  subnet_id      = aws_subnet.database-subnet-1.id
+  route_table_id = aws_route_table.pri-rt.id
+}
+resource "aws_route_table_association" "rt_associate_private" {
+  subnet_id      = aws_subnet.database-subnet-2.id
+  route_table_id = aws_route_table.pri-rt.id
+}
